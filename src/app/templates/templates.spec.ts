931994a3cbf03a11b79abcb5268d0900b773b0fc
kahboom <rachelyordan@gmail.com>
2016-09-28 18:46:53 -0400

    Add templates component

---
+++ src/app/templates/templates.spec.ts
@@ -0,0 +1,35 @@
+import {ActivatedRoute} from '@angular/router';
+import {Component} from '@angular/core';
+import {inject, TestBed} from '@angular/core/testing';
+
+// Load the implementations that should be tested
+import {Templates} from './templates.component';
+
+describe('Templates', () => {
+    // provide our implementations or mocks to the dependency injector
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            // provide a better mock
+            {
+                provide: ActivatedRoute,
+                useValue: {
+                    data: {
+                        subscribe: (fn) => fn({
+                            yourData: 'yolo'
+                        })
+                    }
+                }
+            },
+            Templates
+        ]
+    }));
+    
+    it('should log ngOnInit', inject([Templates], (templates) => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
+        
+        templates.ngOnInit();
+        expect(console.log).toHaveBeenCalled();
+    }));
+    
+});
