40f9b00b606442bd6e2586a63a4338efdc09ce13
kahboom <rachelyordan@gmail.com>
2016-09-28 18:48:04 -0400

    Add integrations component

---
+++ src/app/integrations/integrations.spec.ts
@@ -0,0 +1,35 @@
+import {ActivatedRoute} from '@angular/router';
+import {Component} from '@angular/core';
+import {inject, TestBed} from '@angular/core/testing';
+
+// Load the implementations that should be tested
+import {Integrations} from './integrations.component';
+
+describe('Integrations', () => {
+    // provide our implementations or mocks to the dependency injector
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            // provide a better mock
+            {
+                provide: ActivatedRoute,
+                useValue: {
+                    data: {
+                        subscribe: (fn) => fn({
+                            yourData: 'yolo'
+                        })
+                    }
+                }
+            },
+            Integrations
+        ]
+    }));
+    
+    it('should log ngOnInit', inject([Integrations], (integrations) => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
+        
+        integrations.ngOnInit();
+        expect(console.log).toHaveBeenCalled();
+    }));
+    
+});
