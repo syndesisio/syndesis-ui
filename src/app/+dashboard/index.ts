ed0cda942d648c30a2b4af49e1d2420facac187d
kahboom <rachelyordan@gmail.com>
2016-10-07 22:29:26 -0400

    Add asynchronous support for Dashboard and User

--- src/app/+dashboard/index.ts
+++ src/app/+dashboard/index.ts
@@ -1,1 +1,32 @@
-export * from './dashboard.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { Dashboard } from './dashboard.component.ts';
+
+Logger.get('+Dashboard').debug('`Dashboard` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: Dashboard
+    }
+];
+
+@NgModule({
+    declarations: [
+        Dashboard
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class DashboardModule {
+    static routes = routes;
+}
+
--- src/app/+user/index.ts
+++ src/app/+user/index.ts
@@ -1,1 +1,32 @@
-export * from './user.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { User } from './user.component.ts';
+
+Logger.get('+User').debug('`User` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: User
+    }
+];
+
+@NgModule({
+    declarations: [
+        User
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class UserModule {
+    static routes = routes;
+}
+
