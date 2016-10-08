7152f4c8b739c4c5ecc084748b523139b9c690a0
kahboom <rachelyordan@gmail.com>
2016-10-07 22:33:19 -0400

    Add async support for Templates

--- src/app/+templates/index.ts
+++ src/app/+templates/index.ts
@@ -1,1 +1,32 @@
-export * from './templates.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { Templates } from './templates.component.ts';
+
+Logger.get('+Templates').debug('`Templates` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: Templates
+    }
+];
+
+@NgModule({
+    declarations: [
+        Templates
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class TemplatesModule {
+    static routes = routes;
+}
+
