ea90853a029ee64ec42031876c53e5586467acbb
kahboom <rachelyordan@gmail.com>
2016-10-07 22:34:20 -0400

    Add async support for Admin component

--- src/app/+admin/index.ts
+++ src/app/+admin/index.ts
@@ -1,1 +1,32 @@
-export * from './admin.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { Admin } from './admin.component.ts';
+
+Logger.get('+Admin').debug('`Admin` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: Admin
+    }
+];
+
+@NgModule({
+    declarations: [
+        Admin
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class AdminModule {
+    static routes = routes;
+}
+
