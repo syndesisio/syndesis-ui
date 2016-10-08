edf917c2ea99ec3719127c1c8bd476b43fd818af
kahboom <rachelyordan@gmail.com>
2016-10-07 22:30:38 -0400

    Add asynchronous support for Integrations

--- src/app/+integrations/index.ts
+++ src/app/+integrations/index.ts
@@ -1,1 +1,32 @@
-export * from './integrations.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { Integrations } from './integrations.component.ts';
+
+Logger.get('+Integrations').debug('`Integrations` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: Integrations
+    }
+];
+
+@NgModule({
+    declarations: [
+        Integrations
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class IntegrationsModule {
+    static routes = routes;
+}
+
