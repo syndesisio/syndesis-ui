32e2027a4d19125d79ead20c163177c9f80e7de6
kahboom <rachelyordan@gmail.com>
2016-10-07 22:33:52 -0400

    Add async support for Connections

--- src/app/+connections/index.ts
+++ src/app/+connections/index.ts
@@ -1,1 +1,32 @@
-export * from './connections.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {Logger} from '../log.service';
+
+import { Connections } from './connections.component.ts';
+
+Logger.get('+Connections').debug('`Connections` bundle loaded asynchronously');
+
+// async components must be named routes for WebpackAsyncRoute
+const routes = [
+    { path: '',
+        component: Connections
+    }
+];
+
+@NgModule({
+    declarations: [
+        Connections
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export default class ConnectionsModule {
+    static routes = routes;
+}
+
