7296771702099998b025e8507d31b8f7026493e0
Stan Lewis <slewis@fusesource.com>
2016-10-11 12:41:49 -0400

    Fix compile error after 'npm update'

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+
 import {Logger} from './log.service';
 
 // defer initializing this, otherwise 'Logger' is undefined
