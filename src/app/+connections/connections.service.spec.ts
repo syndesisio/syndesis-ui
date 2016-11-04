dbe43284f9b77b3ccbda5b07c0440fb056661d41
kahboom <rachelyordan@gmail.com>
2016-11-04 12:59:33 -0400

    Add ConnectionService and ConnectionComponent unit tests

---
+++ src/app/+connections/connections.service.spec.ts
@@ -0,0 +1,48 @@
+import {
+  inject,
+  TestBed
+} from '@angular/core/testing';
+
+import {
+  BaseRequestOptions,
+  ConnectionBackend,
+  Http
+} from '@angular/http';
+
+import { MockBackend } from '@angular/http/testing';
+
+import { ConnectionService } from './connection.service';
+
+describe('ConnectionService', () => {
+    beforeEach(() => TestBed.configureTestingModule({
+        providers: [
+            BaseRequestOptions,
+            MockBackend,
+            {
+                provide: Http,
+                useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
+                    return new Http(backend, defaultOptions);
+                },
+                deps: [MockBackend, BaseRequestOptions]
+            },
+            ConnectionService
+        ]}));
+    
+    // Can't set this up until we have the API wired up,
+    // maybe can use a mock backend instead in the meantime?
+    /*
+    it('should have http', inject([ ConnectionService ], (connection: ConnectionService) => {
+        expect(!!connection.http).toEqual(true);
+    }));
+    */
+    
+    it('should get data from the server', inject([ ConnectionService ], (connection: ConnectionService) => {
+        spyOn(console, 'log');
+        expect(console.log).not.toHaveBeenCalled();
+        
+        connection.getAll();
+        expect(console.log).toHaveBeenCalled();
+        expect(connection.getAll()).toEqual({ value: 'AngularClass' });
+    }));
+    
+});
