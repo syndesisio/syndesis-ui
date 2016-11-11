eabc6cf9b5ef0d8a63dcc3894affb9cfbd95be04
kahboom <rachelyordan@gmail.com>
2016-11-11 09:15:07 -0500

    Styling for Create Connection wizard

--- src/app/+connections/library/library.component.ts
+++ src/app/+connections/library/library.component.ts
@@ -9,70 +9,72 @@ import { Router } from '@angular/router';
 var log = Logger.get('+connections/library');
 
 @Component({
-    moduleId: module.id,
-    selector: 'connections-library',
-    encapsulation: ViewEncapsulation.None,
-    styles: [ require('./library.scss') ],
-    templateUrl: './library.html',
-    providers: [ ConnectionService ]
+  moduleId: module.id,
+  selector: 'connections-library',
+  encapsulation: ViewEncapsulation.None,
+  styles: [ require('./library.scss') ],
+  templateUrl: './library.html',
+  providers: [ ConnectionService ]
 })
 export class Library implements OnInit {
 
-    limit = 80;
-    trail = '..';
+  limit = 80;
+  trail = '..';
 
-    listFilter: string;
-    errorMessage: string;
+  listFilter: string;
+  errorMessage: string;
 
-    @Input() connections: IConnection[];
+  @Input() connections: IConnection[];
 
 
-    /**
-     * Constructor.
-     * @param router - Router
-     * @param _connectionService - ConnectionService
-     */
-    constructor(private router: Router,
-                private _connectionService: ConnectionService) {
+  /**
+   * Constructor.
+   * @param router - Router
+   * @param _connectionService - ConnectionService
+   */
+  constructor(private router: Router,
+              private _connectionService: ConnectionService) {
+  }
+
+  ngOnInit() {
+    log.debug('hello `Connections: Library` component');
+
+    this.getConnections();
+  }
+
+  deleteConnection(connection: IConnection, $event: any): void {
+  }
+
+  duplicateConnection(connection: IConnection, $event: any): void {
+  }
+
+  editConnection(connection: IConnection, $event: any): void {
+    if ($event.target.className.indexOf('dropdown-toggle') !== -1) {
+      return;
     }
 
-    ngOnInit() {
-        log.debug('hello `Connections: Library` component');
+    console.log('Connection: ', connection);
 
-        this.getConnections();
+    let link = [ '/detail', connection.name ];
+    this.router.navigate(link);
+  }
+
+  getConnections() {
+    this._connectionService.getAll()
+      .subscribe(
+        connections => this.connections = connections,
+        error => this.errorMessage = <any>error);
+  }
+
+  gotoDetail(connection: IConnection, $event: any): void {
+    if ($event.target.className.indexOf('dropdown-toggle') !== -1) {
+      return;
     }
 
-    deleteConnection(connection: IConnection, $event: any): void {}
+    console.log('Connection: ', connection);
 
-    duplicateConnection(connection: IConnection, $event: any): void {}
-
-    editConnection(connection: IConnection, $event: any): void {
-        if ($event.target.className.indexOf('dropdown-toggle') !== -1) {
-            return;
-        }
-
-        console.log('Connection: ', connection);
-
-        let link = [ '/detail', connection.name ];
-        this.router.navigate(link);
-    }
-
-    getConnections() {
-        this._connectionService.getAll()
-          .subscribe(
-            connections => this.connections = connections,
-            error => this.errorMessage = <any>error);
-    }
-
-    gotoDetail(connection: IConnection, $event: any): void {
-        if ($event.target.className.indexOf('dropdown-toggle') !== -1) {
-            return;
-        }
-
-        console.log('Connection: ', connection);
-
-        let link = [ '/detail', connection.name ];
-        this.router.navigate(link);
-    }
+    let link = [ '/detail', connection.name ];
+    this.router.navigate(link);
+  }
 
 }
