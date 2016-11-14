431393ec79d6e7faaf96b13e528c1a4cc0fa8672
kahboom <rachelyordan@gmail.com>
2016-11-14 11:25:15 -0500

    Detail view

--- src/app/+connections/detail/detail.component.ts
+++ src/app/+connections/detail/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { IConnection } from '../connection.model';
 import { ConnectionService } from '../connection.service';
@@ -8,37 +9,59 @@ import { Logger } from '../../common/service/log';
 var log = Logger.get('+connections/detail');
 
 @Component({
-    moduleId: module.id,
-    selector: 'connections-detail',
-    encapsulation: ViewEncapsulation.None,
-    styleUrls: [ 'detail.scss' ],
-    templateUrl: 'detail.html',
-    providers: [ ConnectionService ]
+  moduleId: module.id,
+  selector: 'connections-detail',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [ 'detail.scss' ],
+  templateUrl: 'detail.html',
+  providers: [ ConnectionService ]
 })
 export class Detail implements OnInit {
 
-    connection: IConnection;
-    errorMessage: string;
+  @Input() connection: IConnection;
+  @Output() close = new EventEmitter();
+  error: any;
+  navigated = false; // true if navigated here
 
-    /**
-     * Constructor.
-     * @param _connectionService - ConnectionService
-     */
-    constructor(private _connectionService: ConnectionService) {
-        this.getConnection();
-    }
+  /**
+   * Constructor.
+   * @param _connectionService - ConnectionService
+   * @param route - ActivatedRoute
+   */
+  constructor(private _connectionService: ConnectionService,
+              private route: ActivatedRoute) {}
 
-    ngOnInit(): void {
-        log.debug('hello `Connections: Detail` component');
-    }
+  ngOnInit(): void {
+    log.debug('hello `Connections: Detail` component');
 
-    getConnection() {
-        /*
-        this._connectionService.get()
+    this.route.params.forEach((params: Params) => {
+      if (params[ 'name' ] !== undefined) {
+        let name = +params[ 'name' ];
+        this.navigated = true;
+        this._connectionService.get(name.toString())
           .subscribe(
-            connections => this.connections = connections,
-            error => this.errorMessage = <any>error);
-            */
-    }
+            connections => this.connection = connections,
+            error => this.error = <any>error);
+      } else {
+        this.navigated = false;
+        this.connection = new IConnection();
+      }
+    });
+  }
+
+  save(): void {
+    this._connectionService
+      .update(this.connection)
+      .then(connection => {
+        this.connection = connection; // saved connection, w/ id if new
+        this.goBack(connection);
+      })
+      .catch(error => this.error = error); // TODO: Display error message
+  }
+
+  goBack(savedConnection: IConnection = null): void {
+    this.close.emit(savedConnection);
+    if (this.navigated) { window.history.back(); }
+  }
 
 }
