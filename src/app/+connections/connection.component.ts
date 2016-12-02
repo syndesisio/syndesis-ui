71beebacb8a3b4677a66b44a28434be0bd67bc8f
kahboom <rachelyordan@gmail.com>
2016-12-02 09:08:04 -0500

    Load Components from Create Connection view

--- src/app/+connections/connection.component.ts
+++ src/app/+connections/connection.component.ts
@@ -2,7 +2,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 
 import {Logger} from '../common/service/log';
 
-var log = Logger.get('+connections');
+let log = Logger.get('+connections');
 
 @Component({
     selector: 'connections',
--- src/app/+connections/create/create.component.ts
+++ src/app/+connections/create/create.component.ts
@@ -1,11 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router'
 
+// Components
+import { IComponent } from '../component.model';
+import { ComponentService } from '../component.service';
+
+// Connections
 import { IConnection } from '../connection.model';
 import { ConnectionService } from '../connection.service';
 
 import { Logger } from '../../common/service/log';
-
 let log = Logger.get('+connections/create');
 
 @Component({
@@ -15,7 +19,7 @@ let log = Logger.get('+connections/create');
   templateUrl: './create.html',
   providers: [ ConnectionService ]
 })
-export class Create implements OnInit {
+export class Create implements OnInit, OnDestroy {
   limit = 60;
   trail = '..';
 
@@ -23,28 +27,58 @@ export class Create implements OnInit {
   listFilter: string;
   errorMessage: string;
 
+  //orderByArray: ['name', 'type'];
+
+  //@Input() connections: IConnection[];
   connections: IConnection[];
   connection: IConnection;
 
+  //@Input() components: IComponent[];
+  components: IComponent[];
+  component: IComponent;
+
+  /**
+   * Constructor.
+   * @param _componentService - ComponentService
+   * @param _connectionService - ConnectionService
+   * @param _router - Router
+   */
   constructor(private _connectionService: ConnectionService,
-              private router: Router) {
-    this.getConnections();
-  }
+              private _componentService: ComponentService,
+              private _router: Router) {}
 
   ngOnInit() {
     log.debug('hello `Connections: Create` component');
 
+    /*
     this._connectionService.getAll()
       .subscribe(connections => this.connections = connections,
         error => this.errorMessage = <any>error);
+        */
+
+    this._componentService.getAll()
+      .subscribe(components => this.components = components,
+        error => this.errorMessage = <any>error);
   }
 
+  ngOnDestroy() {}
 
+  // Components
+  getComponents() {
+    this._componentService.getAll();
+
+    console.log('Components: ' + JSON.stringify(this.component));
+  }
+
+  // Connections
   getConnections() {
     this._connectionService.getAll();
 
     console.log('Connections: ' + JSON.stringify(this.connection));
   }
+
+
+  // Steps
 
   goToStep1() {
     this.currentStep = 1;
